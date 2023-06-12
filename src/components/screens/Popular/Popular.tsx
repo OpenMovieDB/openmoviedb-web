6c4c3be9fa29d15662474d0a91fcc061a0e38f61
Berezhnev Vladimir <vova21473@gmail.com>
2023-06-12 11:57:50 +1000

    fix: fix placement of MovieCards on Popular page

--- src/components/screens/Popular/Popular.tsx
+++ src/components/screens/Popular/Popular.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Box, Flex, Heading } from "@chakra-ui/react";
+import { Box, Flex, Heading, HStack } from "@chakra-ui/react";
 import { MovieCard } from "@/components/MovieCard/MovieCard";
 import Filters from "@/components/Filters/Filters";
 
@@ -10,15 +10,13 @@ const Popular = () => {
   return (
     <>
       <Box className="container" mt="90px">
-        <Heading>Популярное</Heading>
+        <Heading fontWeight="800" fontSize="40px" lineHeight="41px">
+          Популярное
+        </Heading>
 
-        <Flex mt="30px" flexWrap="wrap">
+        <Flex mt="70px" gap="30px" flexWrap="wrap">
           {listOfPopular.map((item, id) => {
-            return (
-              <Box key={id} style={{ marginRight: "30px", marginTop: "30px" }}>
-                <MovieCard item={item} />
-              </Box>
-            );
+            return <MovieCard key={id} item={item} />;
           })}
         </Flex>
         <Filters />
